eeb5ec14cbbad174cc264b3905a475bdb48f115e
teaRain9 <adel.ht@gmail.com>
2022-11-29 14:38:16 -0800

    something

--- routes.js
+++ routes.js
@@ -17,6 +17,7 @@ const requestHandler = (req, res) => {
     }
     if (url === '/message' && method === 'POST' ) {
         const body = [];
+        req.on('')
         req.on('data', (chunk) => {
             console.log(chunk)
             body.push(chunk)
