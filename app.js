60648f640e67f45fd0dc5d19eb286f9adb14b799
Adel Hosseini <adel.ht@gmail.com>
2022-12-06 06:47:46 -0800

    f

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const shopRouter = require('./routes/shop')
-const adminRoutes = require('./routes/admin')
 const path = require("path");
-const rootDir = require('./util/path')
+
+const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use('/admin', adminRoutes)
+const shopRouter = require('./routes/shop')
+const adminRoutes = require('./routes/admin')
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use('/admin', adminRoutes)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', '404.html'))
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000);
