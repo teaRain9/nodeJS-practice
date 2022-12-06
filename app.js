3dcd2179d0f52872a15855b62824ec4678f355f9
Adel Hosseini [AH1004_SA] <AH1004_SA@healthbc.org>
2022-12-05 21:48:15 -0800

    working with html

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const shopRouter = require('./routes/shop')
 const adminRoutes = require('./routes/admin')
+const path = require("path");
+const rootDir = require('./util/path')
 
 const app = express();
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 
 app.use(shopRouter)
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', '404.html'))
+})
 
 app.listen(3000);
 
