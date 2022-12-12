6e85c87e6b1521103ea08f715e46c319de36b4ff
teaRain9 <adel.ht@gmail.com>
2022-12-12 15:54:08 -0800

    commit

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+app.set('view engine', 'ejs')
+app.set('views', 'views') // that's defaults, so with views folder we can delete this line
+
 const shopRouter = require('./routes/shop')
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', {pageTitle: 'Page Not Found'})
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,22 @@
 const path = require("path");
 const express = require('express');
 const rootDir = require('../util/path')
-const router = express.Router();
-const bodyParser = require('body-parser')
 
-router.use(bodyParser.urlencoded({extended: false}))
+
+
+const router = express.Router();
+const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+  res.render('add-product', {pageTitle: 'Add Product'})
 })
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
+  products.push({title: req.body.title})
   res.redirect('/');
 })
 
-module.exports = router;
+exports.routes =  router;
+exports.products = products;
