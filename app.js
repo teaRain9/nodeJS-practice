dc8782108aac0a5aae41b9e0a93ac1f060d279fa
teaRain9 <adel.ht@gmail.com>
2022-12-13 13:56:31 -0800

    working on MVC

--- app.js
+++ app.js
@@ -9,19 +9,17 @@ app.set('view engine', 'ejs')
 app.set('views', 'views') // that's defaults, so with views folder we can delete this line
 
 const shopRouter = require('./routes/shop')
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
+const errorController = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRouter)
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render('404', {pageTitle: 'Page Not Found'})
-})
+app.use(errorController.get404)
 
 app.listen(3000);
 
